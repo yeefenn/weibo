742cb47d682ff83fab770a9bbb9f4a52b5d92f8b
yf <1301714937@qq.com>
2021-02-21 05:51:18 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+      @yield('title','weibo App')
+    </title>
+</head>
+<body>
+ @yield('content')
+</body>
+
+
+</html>
+
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','关于')
+@section('contion')
+ <h1>关于页</h1>
+@stop

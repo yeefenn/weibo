309542154c5a44e99c36d327e8d7bfdd9e59cb4c
yf <1301714937@qq.com>
2021-02-22 11:14:23 +0000

    中文的错误提示

--- config/app.php
+++ config/app.php
@@ -80,7 +80,11 @@ return [
     |
     */
 
+
+    /*
     'locale' => 'en',
+    */
+    'locale' => 'zh_CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -158,7 +162,10 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
+        /*更改中文报错修改
         Illuminate\Translation\TranslationServiceProvider::class,
+        */
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 

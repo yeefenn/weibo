73c6a291057effd28dd789d62295a5a84209748a
yf <1301714937@qq.com>
2021-02-23 12:05:17 +0000

    登陆成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,6 +39,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
         //dump($user);exit;
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }

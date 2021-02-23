4d9486594e6581f33d853f15832707a6fd684c13
yf <1301714937@qq.com>
2021-02-23 16:18:25 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except'=>['show','create','store']
+            'except'=>['show','create','store','index']
         ]);
         $this->middleware('guest',[
             'only'=>['create']
@@ -89,6 +89,14 @@ class UsersController extends Controller
 
     }
 
+
+    public function index()
+    {
+
+        $users = User::paginate(6);
+     return view('users.index',compact('users'));
+
+    }
 
 
 }

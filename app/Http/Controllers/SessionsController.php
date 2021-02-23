fe476c5400876ebe9c861a35c988ebbaab7fd9a6
yf <1301714937@qq.com>
2021-02-23 11:32:02 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsController extends Controller
+{
+    //
+  public function create()
+  {
+     return view('sessions.create');
+
+  }
+
+  public function store(Request $request)
+  {
+      $credentilals = $this->validate($request,[
+          'email'=>'required|email|max:225',
+          'password'=>'required'
+      ]);
+      if (Auth::attempt($credentilals))
+      {
+        session()->flash('success','欢迎回来！');
+        return redirect()->route('users.show',[Auth::user()]);
+
+      }else
+      {
+        session()->flash('danger','很抱歉，您的邮箱和密码不匹配！');
+        return redirect()->back()->withInput();
+
+      }
+
+      return;
+  }
+
+
+
+
+
+
+}

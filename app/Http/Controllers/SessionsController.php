68fb9493e8c171506a69e36f885396639c0f74f6
yf <1301714937@qq.com>
2021-02-23 12:28:00 +0000

    登陆时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
           'email'=>'required|email|max:225',
           'password'=>'required'
       ]);
-      if (Auth::attempt($credentilals))
+      if (Auth::attempt($credentilals,$request->has('remember')))
       {
         session()->flash('success','欢迎回来！');
         return redirect()->route('users.show',[Auth::user()]);

0c0c4763a19b40cf05b643702407b3b3a4a22c4b
yf <1301714937@qq.com>
2021-02-24 15:34:00 +0000

    发送找回密码成功

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,9 @@ Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destroy')->name('logout');
 // 激活邮箱路由
 Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
+// 密码重置
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');

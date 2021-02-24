b2fafb4c35ea049d3c1776472309c7de4d4cd6b3
yf <1301714937@qq.com>
2021-02-24 17:14:35 +0000

    移除邮件发送的from选项，改成qq邮箱发送

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -34,6 +34,7 @@ class SessionsController extends Controller
       ]);
       if (Auth::attempt($credentilals,$request->has('remember')))
       {
+
           if (Auth::user()->activated) {
             session()->flash('success','欢迎回来！');
             $fallback = route('users.show',Auth::user());

70f7eef4a054c43a16ff40772ef80ccff558558a
yf <1301714937@qq.com>
2021-02-26 09:05:16 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,12 +67,18 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    // 查找所有发布过的微博
+    // 查找所有发布过的微博,关注用户的微博
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        // return $this->statuses()
+        //             ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
+
 
     // 用户关注粉丝
     public function followers()
